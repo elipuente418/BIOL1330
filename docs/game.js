96c3f3e902f5f2c1db2e157e9bec766592b359ac
hengolivia <53484921+oliviaaheng@users.noreply.github.com>
2026-02-27 23:59:34 -0500

    Revert "add for checking if the player loses"
    This reverts commit 89fda586af7b7cac9f0755fc5cd0aa0a65d73f60.

--- docs/game.js
+++ docs/game.js
@@ -105,47 +105,27 @@ function slide(row) {
 }
 
 function moveLeft() {
-    let changed = false;
-    for (let r = 0; r < 4; r++) {
-        let result = slide(board[r]);
-        board[r] = result.row;
-        if (result.changed) changed = true;
-    }
-    return changed;
+    for (let r=0; r<4; r++) board[r] = slide(board[r]);
 }
 
 function moveRight() {
-    let changed = false;
-    for (let r = 0; r < 4; r++) {
-        let reversed = board[r].slice().reverse();
-        let result = slide(reversed);
-        board[r] = result.row.reverse();
-        if (result.changed) changed = true;
-    }
-    return changed;
+    for (let r=0; r<4; r++) board[r] = slide(board[r].reverse()).reverse();
 }
 
 function moveUp() {
-    let changed = false;
-    for (let c = 0; c < 4; c++) {
+    for (let c=0; c<4; c++) {
         let col = [board[0][c], board[1][c], board[2][c], board[3][c]];
-        let result = slide(col);
-        for (let r = 0; r < 4; r++) board[r][c] = result.row[r];
-        if (result.changed) changed = true;
+        col = slide(col);
+        for (let r=0; r<4; r++) board[r][c] = col[r];
     }
-    return changed;
 }
 
 function moveDown() {
-    let changed = false;
-    for (let c = 0; c < 4; c++) {
-        let col = [board[0][c], board[1][c], board[2][c], board[3][c]].reverse();
-        let result = slide(col);
-        let newCol = result.row.reverse();
-        for (let r = 0; r < 4; r++) board[r][c] = newCol[r];
-        if (result.changed) changed = true;
+    for (let c=0; c<4; c++) {
+        let col = [board[0][c], board[1][c], board[2][c], board[3][c]];
+        col = slide(col.reverse()).reverse();
+        for (let r=0; r<4; r++) board[r][c] = col[r];
     }
-    return changed;
 }
 
 function hasMoves() {
@@ -176,7 +156,7 @@ document.addEventListener("keydown", e => {
     if (JSON.stringify(board) !== old) {
     addRandomTile();
     drawBoard();
-
+    
         if (!hasMoves()) {
             setTimeout(() => {
                 alert("You lost! Try again?");
